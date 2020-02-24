3bc2f27338c1442a9233b634102f98d33e9c716e
hongdonghyun <hong4367@gmail.com>
2020-02-24 14:26:37 +0900

    setting weatherApp

---
+++ WeatherForecast (Starter)/WeatherForecast/ViewControllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Giftbot on 2020/02/22.
+//  Copyright © 2020 Giftbot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    private let weatherTableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
---
+++ WeatherForecast (Starter)/WeatherForecast/Views/WeatherTableView.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherTableView.swift
+//  WeatherForecast
+//
+//  Created by Hongdonghyun on 2020/02/24.
+//  Copyright © 2020 Giftbot. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableView: UITableView {
+    
+
+}
